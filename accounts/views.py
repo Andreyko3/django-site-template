965f43ace3d34a419770c6daec1b3e1e35119b7b
Andreyko3 <159200209+Andreyko3@users.noreply.github.com>
2024-02-09 21:18:35 +0100

    add singIn form

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render, redirect
-from .forms import SignUpForm
+from .forms import SignUpForm, SignInForm
+from django.contrib.auth import authenticate, login
+
+
+def sign_in(request):
+    if request.method == 'POST':
+        form = SignInForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('success_url')
+            else:
+                form.add_error(None, 'Username or password is incorrect')
+    else:
+        form = SignInForm()
+    return render(request, 'login_template.html', {'form': form})
+
 
 def signup(request):
     if request.method == 'POST':
