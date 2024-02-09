99d6d54afdecf87335261b567f21b785cf1cce83
Andreyko3 <159200209+Andreyko3@users.noreply.github.com>
2024-02-09 21:16:47 +0100

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+class SignInForm(forms.Form):
+    username = forms.CharField(max_length=254)
+    password = forms.CharField(widget=forms.PasswordInput)
+    
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
 
